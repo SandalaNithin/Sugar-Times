a184ab4aa1d745183dc1cb6b9beb05f1b1ac424b
SandalaNithin <sandalanithinkumar123@gmail.com>
2026-05-06 13:36:35 +0530

    solve

--- next.config.js
+++ next.config.js
@@ -12,6 +12,35 @@ const nextConfig = {
       { protocol: "https", hostname: "st-be-kh3k.onrender.com" },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/category/sugar/",
+        destination: "/news/?category=Sugar%20Prices",
+        permanent: true,
+      },
+      {
+        source: "/category/sugar-sector/",
+        destination: "/news/?category=Sugar%20Industry",
+        permanent: true,
+      },
+      {
+        source: "/category/featured/",
+        destination: "/news/?category=Market%20Trends",
+        permanent: true,
+      },
+      {
+        source: "/category/ethanol/",
+        destination: "/news/?category=Ethanol",
+        permanent: true,
+      },
+      {
+        source: "/about-us/",
+        destination: "/about/",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
